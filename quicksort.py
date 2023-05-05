d080d0405e2df0cee6f3ca8affa13da4ad9cd602
Shruti Ghoniya <22b1288@iitb.ac.in>
2023-05-05 17:12:42 +0530

    Quicksort implemented

---
+++ quicksort.py
@@ -0,0 +1,11 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+arr = [3, 6, 2, 8, 1, 9, 4, 5, 7]
+sorted_arr = quicksort(arr)
+print(sorted_arr)
